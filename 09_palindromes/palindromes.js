638f11d1396ce46ceb3f28a77824a9ce3d0e2094
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 18:47:56 +0200

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (inputString) {
+    const cleanString = inputString.toLowerCase().split('').filter(isNotLetter).join('');
 
+    function isNotLetter(char){
+        if(!(char.charCodeAt(0) > 96 && char.charCodeAt(0) < 123)
+            && !(char.charCodeAt(0) > 48 && char.charCodeAt(0) < 58)) return false;
+        return char;
+    };
+
+    const reversedString = cleanString.split("").reverse().join('');
+
+    if(cleanString === reversedString) return true;
+    return false;
 };
+
+palindromes("r3ace3car")
 
 // Do not edit below this line
 module.exports = palindromes;
