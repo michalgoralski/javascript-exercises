fe9f846dc13c1e9b3bf5e968e7ec072bf91ba70b
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 17:28:08 +0200

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, lastNumber) {
+    if(firstNumber < 0 || lastNumber < 0){
+        return "ERROR";
+    }
+    else if(!Number.isInteger(firstNumber) || !Number.isInteger(lastNumber)){
+        return "ERROR";
+    }
 
+    if(firstNumber > lastNumber){
+        const helper = firstNumber;
+        firstNumber = lastNumber;
+        lastNumber = helper;
+    }
+
+    let sum = 0;
+    for(i = firstNumber; i <= lastNumber; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
