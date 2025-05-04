a2c47327118cc555b088b518b8313b7d0150028b
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 19:14:39 +0200

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequencePosition) {
+    sequencePosition = parseInt(sequencePosition);
+    if(sequencePosition < 0) return "OOPS";
+    if(sequencePosition === 0) return 0;
+    if(sequencePosition === 1 || sequencePosition === 2) return 1;
+    if(sequencePosition === 3) return 2;
+    let sequenceSlice = [1, 1, 2];
+    for(i = 0; i < sequencePosition - 3; i++){
+        let currentNumber =  sequenceSlice[sequenceSlice.length - 2] + sequenceSlice[sequenceSlice.length - 1]
+        sequenceSlice.shift();
+        sequenceSlice.push(currentNumber);
+    }
+    return sequenceSlice.pop();
 };
+
+fibonacci(8);
 
 // Do not edit below this line
 module.exports = fibonacci;
