9c10d6d3e6b90100ef6812d81d5932eb6a71e121
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 17:11:33 +0200

    Solve exercises 01-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    let outputArray = inputArray;
+    elementsToRemove.forEach(element => {
+        outputArray = outputArray.filter((item) => item !== element);
+        console.log(outputArray)
+    });
+    return outputArray;
 };
+
+// removeFromArray([1, 2, 3, 4], 1, 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
