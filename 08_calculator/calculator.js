6b81ab986600d8944865881b8a7bbb7ddc9f9536
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 18:10:03 +0200

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((acc, cur) => acc + cur, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((acc, cur) => acc * cur, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a){
+  let factorial = 1;
+  for(i = 1; i <= a; i++) factorial *= i;
+  return factorial;
 };
 
 // Do not edit below this line
