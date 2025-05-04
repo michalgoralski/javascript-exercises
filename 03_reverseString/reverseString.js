57495b199eb784281998166fb7c3e39424767a59
michal.goralski <110466028+michalgoralski@users.noreply.github.com>
2025-05-04 17:12:06 +0200

    Correct last commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(inputString) {
+    let outputString = ''
+    for(i = 0; i < inputString.length; i++){
+        outputString += inputString.charAt(inputString.length - 1 - i);
+    }
+    return outputString;
 };
 
 // Do not edit below this line
